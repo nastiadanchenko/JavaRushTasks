df038c6cce8be55a28a906e23b0bc3546d1af3d2
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-03-20 21:20:25 +0500

    Task0812
    Задача "Cамая длинная последовательность" решена.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -1,26 +1,31 @@
 package com.javarush.task.task08.task0812;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /* 
 Cамая длинная последовательность
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int rdr = Integer.parseInt(reader.readLine());
+        Scanner scanner = new Scanner(System.in);
         List<Integer> list = new ArrayList<>();
-        int j = 1;
-        int k = 1;
-        for (int i = 1; i <5 ; i++) {
-            list.add(rdr);
-            if (list.get(i).equals(list.get(i-1))) {j++;
-            } else if (k<j) {k=j; j=1;}
-            System.out.println(k);
+        int max = 1;
+        int current = 1;
+        for (int i = 0; i <10; i++) {
+            list.add(scanner.nextInt());
         }
+        for (int i = 1; i <list.size() ; i++) {
+            if (list.get(i).equals(list.get(i-1))){current++;
+                if(current>max){max = current;
+                }
+
+            }else current = 1;
+        }
+
+        System.out.println(max);
+
     }
 }
--- 1.JavaSyntax/src/com/javarush/task/task08/task0825/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0825/Solution.java
@@ -5,17 +5,17 @@ package com.javarush.task.task08.task0825;
 */
 
 public class Solution {
-    public int A = 5;
+    public static int A = 5;
     public static int B = 2;
 
     public int C = A * B;
     public static int D = B * A;
 
     public static void main(String[] args) {
-        System.out.println("hjhhgvv");
+
     }
 
-    public static int getValue() {
+    public  int getValue() {
         return D;
     }
 
