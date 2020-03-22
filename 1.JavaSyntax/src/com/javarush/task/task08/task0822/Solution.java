e766c377169b5f9c46f7f84be8e8eb020bee4080
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-03-22 21:38:20 +0500

    Task 0822
    Task 0822, 0532, 0531, 0530, 0529

--- 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task08.task0822;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /* 
@@ -14,12 +18,17 @@ public class Solution {
     }
 
     public static int getMinimum(List<Integer> array) {
-        // Найти минимум тут
-        return 0;
+
+        return Collections.min(array);
     }
 
     public static List<Integer> getIntegerList() throws IOException {
-        // Создать и заполнить список тут
-        return null;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> array = new ArrayList<>();
+        int n = Integer.parseInt(reader.readLine());
+        for (int i = 0; i <n; i++) {
+            array.add(Integer.parseInt(reader.readLine()));
+        }
+        return array;
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task08.task0827;
+
+/* 
+Работа с датой
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        return true;
+    }
+}
