3fe8c919640f89712d5c343d4cf6485cdf2153ba
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-03-30 12:18:25 +0500

    Tasck 0613-0723

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task07.task0712;
+
+import java.io.IOException;
+
+/* 
+Самые-самые
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+    }
+}
