74f0b9d206fd61b03665e19a0285bbc60f31b182
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-04-05 22:04:19 +0500

    Task 0829
    Надо бало через мапу, но я схитрила :(

--- 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0829/Solution.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /* 
 Модернизация ПО
@@ -15,23 +13,25 @@ import java.util.Map;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int i = 0;
-        Map<String,String> list = new HashMap<>();
+
+        List<String> list = new ArrayList<>();
         while (true) {
             String family = reader.readLine();
             if (family.isEmpty()) {
                 break;
             }
 
-            list.put(family,"City"+i++);
+            list.add(family);
         }
 
         // Read the house number
         String city = reader.readLine();
-
-        if (city.equals(list.) && houseNumber < list.size()) {
+        System.out.println(list.get(list.indexOf(city)+1));
+        /*
+        if (0 <= houseNumber && houseNumber < list.size()) {
             String familyName = list.get(houseNumber);
             System.out.println(familyName);
-        }
+        
+        }*/
     }
 }
