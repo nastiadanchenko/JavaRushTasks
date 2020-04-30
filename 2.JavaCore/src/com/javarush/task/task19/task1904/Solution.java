1af25b58b26fbc0b8e18f7056ddf3de68063881d
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-04-30 11:45:58 +0500

    Task1901-1903

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task19.task1904;
+
+/* 
+И еще один адаптер
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class PersonScannerAdapter {
+
+    }
+}
