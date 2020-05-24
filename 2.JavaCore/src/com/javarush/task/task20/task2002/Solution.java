8aa785befbc73a4469ee481ec4b880bc5d99d634
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-05-25 01:59:38 +0500

    Tesk1322-1324

--- 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task20.task2002;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /* 
@@ -18,13 +20,16 @@ public class Solution {
 
             JavaRush javaRush = new JavaRush();
             //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+            javaRush.users.add(new User());
+            javaRush.users.add(new User());
+            javaRush.users.add(new User());
             javaRush.save(outputStream);
             outputStream.flush();
 
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
             //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
-
+            System.out.println(javaRush.equals(loadedObject));
             outputStream.close();
             inputStream.close();
 
@@ -42,10 +47,39 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            PrintWriter pw = new PrintWriter(outputStream);
+            int len = users.size();
+            if (len==0){
+                System.out.println("User list is empty");
+                return;
+            }
+            int nullCount = Collections.frequency(this.users, null);
+            for (int i = 0; i <len ; i++) { // записывать, пропуская null объекты
+                User u = users.get(i);
+                if (u!=null){
+                    pw.println(u.getFirstName());
+                    pw.println(u.getLastName());
+                    pw.println(u.getBirthDate().getTime());
+                    pw.println(u.isMale());
+                    pw.println(u.getCountry());
+                }
+
+            }
+            pw.flush();
         }
 
         public void load(InputStream inputStream) throws Exception {
             //implement this method - реализуйте этот метод
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            while (br.ready()){
+                User user = new User();
+                user.setFirstName(br.readLine());
+                user.setLastName(br.readLine());
+                user.setBirthDate(new Date(Long.parseLong(br.readLine())));
+                user.setMale(Boolean.parseBoolean(br.readLine()));
+                user.setCountry(User.Country.valueOf(br.readLine()));
+                users.add(user);
+            }
         }
 
         @Override
