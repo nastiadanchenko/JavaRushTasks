7c3fac9d2ed3d78fe2bc741c74f6b20eee909070
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-06-17 00:48:23 +0500

    Task22

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2107/Solution.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /* 
 Глубокое клонирование карты
 */
-public class Solution {
+public class Solution implements Cloneable{
 
     public static void main(String[] args) {
         Solution solution = new Solution();
@@ -14,7 +14,7 @@ public class Solution {
         solution.users.put("Zapp", new User(41, "Zapp"));
         Solution clone = null;
         try {
-            clone = solution.clone();
+            clone = (Solution) solution.clone();
             System.out.println(solution);
             System.out.println(clone);
 
@@ -28,13 +28,49 @@ public class Solution {
 
     protected Map<String, User> users = new LinkedHashMap();
 
-    public static class User {
+    public Object clone() throws CloneNotSupportedException {
+        Solution solutionClone = new Solution();
+       // solutionClone.users = this.users; - неверно
+        for (Map.Entry<String,User> x:this.users.entrySet()
+             ) {
+            int age = x.getValue().age;
+            String name = x.getValue().name;
+            solutionClone.users.put(x.getKey(), new User(age, name));
+        }
+        return solutionClone;
+    }
+
+    public static class User implements Cloneable{
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            User userClone = new User(this.age, this.name);
+            return userClone;
+        }
+
         int age;
         String name;
 
         public User(int age, String name) {
             this.age = age;
             this.name = name;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            result = age;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj==this) return true;
+            if(!(obj instanceof User)) return false;
+            User user = (User) obj;
+            if (age!=user.age) return false;
+            if(name!=null ? !name.equals(user.name): user.name!=null) return false;
+            return true;
         }
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by User on 17.06.2020.
+ */
+public class Horse {
+}
