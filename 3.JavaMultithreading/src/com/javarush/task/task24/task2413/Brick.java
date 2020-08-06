5f2f48a785ebca4d6fe74b01d2b72a048334a1d6
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-08-06 14:37:08 +0500

    Task23-2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task24.task2413;
+
+/**
+ * Created by User on 05.08.2020.
+ */
+public class Brick extends BaseObject {
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
