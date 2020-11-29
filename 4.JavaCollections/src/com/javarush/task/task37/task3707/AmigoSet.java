fb8147cd72c97a22f532496e7d8fe6e9ee227654
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2020-11-30 00:09:48 +0500

    Task34

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 import java.util.function.Consumer;
@@ -20,11 +23,47 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
         int capasity = 16 > (collection.size() / .75f) ? 16 : (int) Math.ceil(collection.size() / .75f);
         this.map = new HashMap<>(capasity);
         for (E element : collection
-                ) {
+        ) {
             map.put(element, PRESENT);
         }
     }
 
+    private void writeObject(ObjectOutputStream outputStream) {
+        try {
+            outputStream.defaultWriteObject();
+
+            outputStream.writeInt(HashMapReflectionHelper.callHiddenMethod(map, "capacity"));
+            outputStream.writeFloat(HashMapReflectionHelper.callHiddenMethod(map,"loadFactor"));
+            outputStream.writeInt(map.keySet().size());
+
+            for (E e: map.keySet()
+                 ) {
+                outputStream.writeObject(e);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private  void readObject(ObjectInputStream inputStream){
+        try {
+            inputStream.defaultReadObject();
+
+            int capacity = inputStream.readInt();
+            float loadFactor = inputStream.readFloat();
+            int size = inputStream.readInt();
+
+            map = new HashMap<>(capacity, loadFactor);
+
+            for (int i = 0; i < size; i++) {
+                E e = (E) inputStream.readObject();
+                map.put(e,PRESENT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public boolean add(E e) {
         return map.put(e, PRESENT) == null;
@@ -48,7 +87,7 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
 
     @Override
     public boolean remove(Object o) {
-        return  this.map.remove(o)==PRESENT;
+        return this.map.remove(o) == PRESENT;
     }
 
     @Override
@@ -73,4 +112,5 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
         }
 
     }
+
 }
