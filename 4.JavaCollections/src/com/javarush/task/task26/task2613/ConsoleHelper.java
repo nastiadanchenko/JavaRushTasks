29a905c9776ad0a75d39d02f9379a2b2a88cca14
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-07-30 20:32:56 +0500

    Task 2613(3-4)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -7,14 +7,51 @@ import java.io.InputStreamReader;
 public class ConsoleHelper {
     private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void writeMessage(String message){
+    public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static String readString() throws IOException {
-        String str = bis.readLine();
-        bis.close();
-
-        return str;
+    public static String readString() {
+        try {
+          String  str = bis.readLine();
+            return str;
+        } catch (IOException e) {
+        }
+        return null;
     }
+
+    public static String askCurrencyCode() {
+       
+        while (true) {
+            writeMessage("Введите код валюты");
+           String val = readString();
+            if (val == null || val.trim().length()!=3) {
+                writeMessage("Данные не корректны");
+                continue;
+            }
+            return val.trim().toUpperCase();
+        }
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        while (true) {
+            writeMessage("Bведите два целых положительных числа.\n" +
+                    "Первое число - номинал, второе - количество банкнот");
+            String s = readString();
+            String[] strArray = null;
+            if (s == null || (strArray = s.split(" ")).length != 2) {
+                writeMessage("Пожалуйста, введите корректные данные");
+            }else {
+                try {
+                    if (Integer.parseInt(strArray[0]) <= 0 || Integer.parseInt(strArray[1]) <= 0)
+                        writeMessage("Пожалуйста, введите корректные данные");
+                } catch (NumberFormatException e) {
+                    writeMessage("Пожалуйста, введите корректные данные");
+                    continue;
+                }
+                return strArray;
+            }
+        }
+    }
+
 }
