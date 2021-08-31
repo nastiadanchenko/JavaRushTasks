a728ec5527198afa30b88327336f0d720839396f
nastiadanchenko <43606312+nastiadanchenko@users.noreply.github.com>
2021-08-31 21:17:08 +0500

    Task 3410(13) add nested class KeyHandler class's Field

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class Field extends JPanel {
@@ -13,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+
+        addKeyListener(new KeyHandler());
+        setFocusable(true);
     }
 
     @Override
@@ -27,4 +33,24 @@ public class Field extends JPanel {
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
+
+public class KeyHandler extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+        switch (i) {
+            case KeyEvent.VK_LEFT: 
+                eventListener.move(Direction.LEFT);
+            case KeyEvent.VK_RIGHT:
+                eventListener.move(Direction.RIGHT);
+            case KeyEvent.VK_UP:
+                eventListener.move(Direction.UP);
+            case KeyEvent.VK_DOWN:
+                eventListener.move(Direction.DOWN);
+            case KeyEvent.VK_R:
+                eventListener.restart();
+        }
+    }
 }
+}
+
