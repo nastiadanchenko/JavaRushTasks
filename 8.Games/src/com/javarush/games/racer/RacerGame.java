37c9764b9329b735f294ef3cd7415c5a4c671143
Anastasia Danchenko <nastiadanchenko@gmail.com>
2021-12-08 13:35:16 +0500

    Game racer (1-7)

---
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -0,0 +1,54 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class RacerGame extends Game {
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
+    private PlayerCar player;
+
+    private RoadMarking roadMarking;
+
+    @Override
+    public void initialize() {
+        showGrid(false);
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void createGame() {
+        roadMarking = new RoadMarking();
+        player = new PlayerCar();
+        drawScene();
+    }
+
+    private void drawScene() {
+        drawField();
+        roadMarking.draw(this);
+        player.draw(this);
+    }
+
+    private void drawField() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == CENTER_X) {
+                    setCellColor(x, y, Color.WHITE);
+                } else if (x >= ROADSIDE_WIDTH && x < WIDTH - ROADSIDE_WIDTH) {
+                    setCellColor(x, y, Color.DIMGREY);
+                } else {
+                    setCellColor(x, y, Color.GREEN);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void setCellColor(int x, int y, Color color) {
+        if (x > WIDTH - 1 || x < 0 || y < 0 || y > HEIGHT - 1) {
+            return;
+        }
+        super.setCellColor(x, y, color);
+    }
+}
